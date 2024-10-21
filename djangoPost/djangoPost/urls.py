9ad9ef553edeeb76a69018db568e15b42f9366c5
dvYurchenko <yurchenko.dasha13@gmail.com>
2024-10-21 21:27:46 +0500

    Cоздала Users. Все красиво нужно будет оформить, но пока работает

--- djangoPost/djangoPost/urls.py
+++ djangoPost/djangoPost/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path,include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Posts.urls'))
+    path('',include('Posts.urls')),
+    path('users/', include('users.urls'))
 
 ]
 
