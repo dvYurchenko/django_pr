8e6340c29c8c4cc73c012dfe47cd082a3b2a2b36
dvYurchenko <yurchenko.dasha13@gmail.com>
2024-10-23 20:59:39 +0500

    Сегодня сделали чтобы посты удалялись (или не удалялись)! Было уже лучше)))

--- djangoPost/Posts/urls.py
+++ djangoPost/Posts/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     #path('', Home.as_view(), name='home')
     path('', views.home, name='home'),
     path('posts/create', views.create_post, name='post-create'),
-    path('about/', views.about, name = 'about')
+    path('about/', views.about, name = 'about'),
+    path('posts/edit/<int:id>', views.edit_post, name='post-edit'),
+    path('posts/delete/<int:id>', views.delete_post, name='post-delete')
 ]
--- djangoPost/Posts/views.py
+++ djangoPost/Posts/views.py
@@ -1,9 +1,14 @@
 #from lib2to3.fixes.fix_input import context
+from lib2to3.fixes.fix_input import context
 
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Posts
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def create_post(request):
     if request.method == 'GET':
         context = {'form': PostForm()}
@@ -22,6 +27,31 @@ def home(request):
     return render(request, 'posts/postshome.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    return  render(request, 'blog/about.html', context)
 
+@login_required
+def edit_post(request, id):
+    post = get_object_or_404(Posts, id=id)
+    if request.method == 'GET':
+        context = {'form': PostForm(instance=post), 'id':id}
+        return render(request, 'posts/posts_form.html', context)
+    elif request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пост успешно отредактирован!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Проверь введенные данные. Ошибка!')
+            return render(request, 'posts/posts_form.html', {'form':form})
 
+@login_required
+def delete_post(request, id):
+    post = get_object_or_404(Posts, id=id)
+    context = {'post': post}
+    if request.method == 'GET':
+        return render(request, 'posts/posts_delete.html', context)
+    elif request.method == 'POST':
+        post.delete()
+        messages.success(request, 'Пост успешно удален!')
+        return redirect('home')
